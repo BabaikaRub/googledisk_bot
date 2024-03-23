4edfa05d0e249f9944a63962df9867a1d63f4a77
BabaikaRub <rodionov.mihail02@mail.ru>
2024-03-23 11:47:57 +0300

    add auto ver fot google

--- bot.py
+++ bot.py
@@ -18,7 +18,24 @@ class FSMFiles(StatesGroup):
 
 # Проходим аунтефикацию в гугле
 gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
+
+gauth.LoadCredentialsFile("mycreds.txt")
+
+
+if gauth.credentials is None:
+    gauth.GetFlow()
+    gauth.flow.params.update({'access_type': 'offline'})
+    gauth.flow.params.update({'approval_prompt': 'force'})
+    gauth.LocalWebserverAuth()
+
+elif gauth.access_token_expired:
+    gauth.Refresh()
+else:
+    gauth.Authorize()
+
+gauth.SaveCredentialsFile("mycreds.txt")  
+
+drive = GoogleDrive(gauth)
 
 
 # Работа с гугл диском
